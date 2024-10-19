aca5e54f9c2fea372e97826f495da120a1c2035a
haaymonster <imhayleylee@gmail.com>
2024-10-19 19:07:42 +0900

    :sparkles: auth page and button component

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,20 +3,25 @@ import MainPage from "../pages/Main";
 import RunListPage from "../pages/Runlist";
 import Errorpage from "../pages/errorPage";
 import App from "../App";
+import Login from "../pages/Auth/Login";
 
 const router = createBrowserRouter([
 	{
-    element : <App/>,
-    children: [
-      {
-        path : '/',
-        element: <MainPage/>
-      },
-      {
-        path : 'runList',
-        element  : <RunListPage/>
-      }
-    ],
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <MainPage />,
+			},
+			{
+				path: "runList",
+				element: <RunListPage />,
+			},
+			{
+				path: "Login",
+				element: <Login />,
+			},
+		],
 		errorElement: <Errorpage />,
 	},
 ]);
