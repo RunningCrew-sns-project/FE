cbb3cb7a6cd53a98af9cde41af3edb86bbb7f566
haaymonster <imhayleylee@gmail.com>
2024-10-25 22:45:10 +0900

    무한 스크롤 컴포넌트

---
+++ src/components/InfiniteScroll.tsx
@@ -0,0 +1,59 @@
+import { useCallback, useEffect } from "react";
+import { BeatLoader } from "react-spinners";
+
+interface Props {
+	isLastPage: boolean; //마지막 페이지인지
+	fetch: () => void; // 페이지 하단에 도달시 실행할 함수
+}
+
+//
+
+const InfiniteScroll = (props: Props) => {
+	const handleScroll = useCallback(() => {
+		if (
+			window.innerHeight + document.documentElement.scrollTop >=
+			document.documentElement.offsetHeight - 30
+		) {
+			if (!props.isLastPage) {
+				props.fetch();
+				console.log(
+					window.innerHeight,
+					document.documentElement.scrollTop,
+					document.documentElement.offsetHeight,
+				);
+			}
+		}
+	}, [props]);
+
+	const debounce = (func: Function, delay: number) => {
+		let timeoutId = null;
+		return (...args: any[]) => {
+			if (timeoutId) {
+				clearTimeout(timeoutId);
+			}
+			timeoutId = setTimeout(() => {
+				func(...args);
+			}, delay);
+		};
+	};
+
+	useEffect(() => {
+		const debouncedHandleScroll = debounce(handleScroll, 300); // 300ms 지연
+		window.addEventListener("scroll", debouncedHandleScroll);
+		return () => {
+			window.removeEventListener("scroll", debouncedHandleScroll);
+		};
+	}, [handleScroll]);
+
+	return (
+		<div className="w-full flex justify-center">
+			{props.isLastPage ? (
+				"--End--"
+			) : (
+				<BeatLoader loading={true} color="#C0FF00" />
+			)}
+		</div>
+	);
+};
+
+export default InfiniteScroll;
