47e72e24d61b978d771f1980c632cee0b6893783
uniend5959 <uniend5959@gmail.com>
2024-11-23 12:14:10 +0900

    '타입스크립트에러'

--- src/pages/Crew/PostList.tsx
+++ src/pages/Crew/PostList.tsx
@@ -13,7 +13,7 @@ import { useEffect, useState } from "react";
 interface PostListProps {
 	items: Item[];
 	setPage: (page: number) => void;
-	selectedCrewId: string | number | undefined;
+	selectedCrewId: string ;
 	setStartDate: (startDate: Date | null) => void;
 	setArea: (area: string) => void;
 	setSortOrder: (sortOrder: string) => void;
