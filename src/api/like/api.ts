43a8d1f5a9fe518282bf129d13d22b2362b44598
hyeon-aa <ahhyun1120@naver.com>
2024-11-24 14:09:55 +0900

    feat like

--- src/api/like/api.ts
+++ src/api/like/api.ts
@@ -6,33 +6,75 @@ export const changeblogLike = async (blogId: number) => {
   return response;
 };
 
-export const useLikeMutation = () => {
+export const useLikeMutation = (blogId: number) => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: changeblogLike, 
     onMutate: async (blogId: number) => {
       await queryClient.cancelQueries({queryKey:['blogs']});
+      await queryClient.cancelQueries({ queryKey: ['blogdetail', blogId] });
 
       const previousBlogs = queryClient.getQueryData(['blogs']);
-      queryClient.setQueryData({queryKey:['blogs']}, (old: any = []) =>
-        old.map((blog: any) =>
-          blog.id === blogId
-            ? {
-                ...blog,
-                liked: !blog.liked,
-              }
-            : blog
-        )
-      );
-      return { previousBlogs };
+      const previousBlogDetail = queryClient.getQueryData(['blogdetail', blogId]);
+
+      queryClient.setQueryData(['blogs'], (old: any = { pages: [] }) => {
+        return {
+          ...old,
+          pages: old.pages.map((page: any) => {
+            return {
+              ...page,
+              data: {
+                ...page.data,
+                success: {
+                  ...page.data.success,
+                  responseData: {
+                    ...page.data.success.responseData,
+                    currentScrollItems: page.data.success.responseData.currentScrollItems.map((blog: any) =>
+                      blog.blogId === blogId
+                        ? {
+                            ...blog,
+                            liked: !blog.liked, 
+                          }
+                        : blog
+                    ),
+                  },
+                },
+              },
+            };
+          }),
+        };
+      });
+      queryClient.setQueryData(['blogdetail', blogId], (oldData: any) => {
+        if (oldData && oldData.data && oldData.data.success && oldData.data.success.responseData) {
+          const isLiked = oldData.data.success.responseData.liked;
+          return {
+            ...oldData,
+            data: {
+              ...oldData.data,
+              success: {
+                ...oldData.data.success,
+                responseData: {
+                  ...oldData.data.success.responseData,
+                  liked: !oldData.data.success.responseData.liked, 
+                  likeCount: isLiked ? oldData.data.success.responseData.likeCount - 1 : oldData.data.success.responseData.likeCount + 1,
+                },
+              },
+            },
+          };
+        }
+        return oldData;
+      });
+      return { previousBlogs, previousBlogDetail };
     },
     onError: (err, context :any ) => {
-      queryClient.setQueryData({queryKey:['blogs']}, context.previousBlogs);
+      queryClient.setQueryData(['blogs'], context.previousBlogs);
+      queryClient.setQueryData(['blogdetail', context.previousBlogDetail?.blogId], context.previousBlogDetail);
       console.log(err)
     },
     onSettled: () => {
       queryClient.invalidateQueries({queryKey:['blogs']});
+      queryClient.invalidateQueries({ queryKey: ['blogdetail', blogId] });
     },
   });
 };
